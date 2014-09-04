098236d98641149111b76ae6f885cfb8894ef36d
Ngoc Tran <tran.mai.ngoc@gmail.com>
2014-09-04 10:15:34 -0500

    moved PlayerMane to Player.scala

---
+++ src/main/scala/puertorico/Player.scala
@@ -0,0 +1,23 @@
+package puertorico
+import akka.actor._
+import akka.event.LoggingReceive
+
+case object StartGame
+
+class PlayerMane extends Actor {
+  def receive = LoggingReceive {
+    case ChooseRole => {
+      //trigger some role selection
+      println("I need to pick a role")
+      //val role = Prospector
+      //sender ! RoleChosen(role)
+    }
+    case SelectGoodToProduce => {
+      //trigger some good selection
+      println("I need to pick a good")
+      //val good = Corn
+      //sender ! GoodSelected(Corn)
+    }
+  }
+}
+
